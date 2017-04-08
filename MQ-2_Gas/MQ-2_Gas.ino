d768e316a5e984b28888c9a3b2cb97293a7b6c92
Unknown <eduardo.flores@cetys.edu.mx>
2017-04-08 15:25:16 -0700

    + led proof (GAS)

--- MQ-2_Gas/MQ-2_Gas.ino
+++ MQ-2_Gas/MQ-2_Gas.ino
@@ -1,12 +1,22 @@
 const int gasPIN = A0;
-
+const int Referencia = 500;
+const int led = 13;
 void setup() {
   // put your setup code here, to run once:
 Serial.begin(9600);
+pinMode(led,OUTPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-Serial.println(analogRead(gasPIN));
+int aire = analogRead(gasPIN));
+if(aire>Referencia){
+  digitalWrite(led,HIGH);
+  delay(200);
+  Serial.println(aire);
 delay(1000);
+digitalWrite(led,LOW);
+
+  }
+
 }
